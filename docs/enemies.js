4125fd9957e7a77d6b41e48c5a4f4d3a7d04834d
zr <xs24368@bristol.ac.uk>
2025-02-02 23:15:00 +0000

    deploy

---
+++ docs/enemies.js
@@ -0,0 +1,107 @@
+class Enemy {
+    constructor(){
+        this.frameX = 0;
+        this.frameY = 0;
+        this.fps = 20;
+        this.frameInterval = 1/this.fps;
+        this.frameTimer = 0;
+        this.markedForDeletion = false;
+    }
+    update(deltaTime){
+        this.x -= (this.vx + this.game.speed) * deltaTime;
+        this.y += this.vy;
+        if (this.frameTimer > this.frameInterval){
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame){
+                this.frameX++;
+            }
+            else{
+                this.frameX = 0;
+            }
+        }
+        else {
+            this.frameTimer += deltaTime;
+        }
+        //check if off screen
+        if (this.x + this.width < 0){
+            this.markedForDeletion = true;
+        }
+    }
+    draw(context){
+        if (this.game.debug) {
+            context.strokeRect(this.x, this.y, this.width, this.height);
+        }
+        context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+    }
+}
+
+export class FlyingEnemy extends Enemy {
+    constructor(game){
+        super();
+        this.game = game;
+        this.width = 60;
+        this.height = 44;
+        this.x = this.game.width + Math.random() * this.game.width * 0.5;
+        this.y = Math.random() * this.game.height * 0.5;
+        this.vx = 200;
+        this.vy = 0;
+        this.maxFrame = 5;
+        this.image = document.getElementById('enemy_fly');
+        this.angle = 0;
+        this.va = Math.random() * 10 + 10
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+        this.angle += this.va * deltaTime;
+        this.y += 100 * Math.sin(this.angle) * deltaTime;
+    }
+
+}
+
+export class GroundEnemy extends Enemy {
+    constructor(game){
+        super();
+        this.game = game;
+        this.width = 60;
+        this.height = 87;
+        this.x = this.game.width - 20;
+        this.y = this.game.height - this.height - this.game.groundMargin;
+        this.image = document.getElementById('enemy_plant');
+        this.vx = 0;
+        this.vy = 0;
+        this.maxFrame = 1;
+    }
+    
+}
+
+export class ClimbingEnemy extends Enemy {
+    constructor(game){
+        super();
+        this.game = game;
+        this.width = 120;
+        this.height = 144;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height * 0.5;
+        this.image = document.getElementById('enemy_spider_big');
+        this.vx = 0;
+        this.vy = Math.random() > 0.5 ? 1 : -1;
+        this.maxFrame = 5;
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+        if (this.y > this.game.height - this.height - this.game.groundMargin){
+            this.vy *= -1;
+        }
+        if(this.y < -this.height){
+            this.markedForDeletion = true;
+        }
+    }
+    draw(context){
+        super.draw(context);
+        context.beginPath();
+        context.moveTo(this.x + this.width/2, 0);
+        context.lineTo(this.x + this.width/2, this.y + 50);
+        context.stroke();
+    }
+    
+}
---
+++ docs/player.js
@@ -0,0 +1,115 @@
+import { Sitting, Running, Jumping, Falling, Rolling, Diving, Hit} from './playerStates.js';
+import { CollisionAnimation } from './collisionAnimation.js';
+
+export class Player {
+    constructor(game) {
+        this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y = this.game.height - this.height - this.game.groundMargin;
+        this.image = document.getElementById('player');
+        this.vx = 0;
+        this.maxVx = 600;
+        this.vy = 0;
+        this.gravity = 2500;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.maxFrame = 5;
+        this.fps = 20;
+        this.frameInterval = 1 / this.fps;
+        this.frameTimer = 0;
+        this.states = [new Sitting(this.game), new Running(this.game), new Jumping(this.game),
+             new Falling(this.game), new Rolling(this.game), new Diving(this.game), new Hit(this.game)];
+    }
+
+    draw(context) {
+        if (this.game.debug) {
+            context.strokeRect(this.x, this.y, this.width, this.height);
+        }
+
+        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+    update(input, deltaTime) {
+        this.checkCollision();
+        this.currentState.handleInput(input);
+        //horizontal movement
+        if (input.includes('d') && (this.currentState !== this.states[6])) {
+            this.vx = this.maxVx;
+        }
+        else if (input.includes('a') && (this.currentState !== this.states[6])) {
+            this.vx = -this.maxVx;
+        }
+        else {
+            this.vx = 0;
+        }
+        this.x += this.vx * deltaTime;
+        //boundary check
+        if (this.x < 0) {
+            this.x = 0;
+        }
+        if (this.x > this.game.width - this.width) {
+            this.x = this.game.width - this.width;
+        }
+        //vertical
+        this.y += this.vy * deltaTime;
+        if (!this.onGround()) {
+            this.vy += this.gravity * deltaTime;
+        }
+        else {
+            this.vy = 0;
+        }
+        //vertical boundary check
+        if(this.y > this.game.height - this.height - this.game.groundMargin) {
+            this.y = this.game.height - this.height - this.game.groundMargin;
+        }
+        //sprite animation
+        if (this.frameTimer > this.frameInterval) {
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame) {
+                this.frameX++;
+            }
+            else {
+                this.frameX = 0;
+            }
+        }
+        else {
+            this.frameTimer += deltaTime;
+        }
+
+
+    }
+
+    onGround() {
+        return this.y >= this.game.height - this.height - this.game.groundMargin;
+    }
+
+    setState(state, speed) {
+        this.game.speed = speed;
+        this.currentState = this.states[state];
+        this.currentState.enter();
+    }
+
+    checkCollision(){
+        this.game.enemies.forEach(enemy => {
+            if (
+                enemy.x < this.x + this.width &&
+                enemy.x + enemy.width > this.x &&
+                enemy.y < this.y + this.height &&
+                enemy.y + enemy.height > this.y
+            ){
+                if (this.currentState === this.states[4] || this.currentState === this.states[5]){
+                    enemy.markedForDeletion = true;
+                    this.game.collisions.push(new CollisionAnimation(this.game, enemy.x + enemy.width/2, enemy.y + enemy.height/2));
+                    this.game.score++;
+                }
+                else{
+                    this.setState(6, 0);
+                }
+            }
+            else{
+
+            }
+        });
+    }
+}
